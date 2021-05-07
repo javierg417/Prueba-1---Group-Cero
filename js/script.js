477fc8883baaff4a7226372192da38d6b8360a9f
javierg417 <javiergallardo417@gmail.com>
2021-05-06 22:38:59 -0400

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,18 +1,73 @@
-function mensaje(){
-    alert('hola mundo')
+function ValidarFomurlarioPublicacion() {
+    var resp = validarNombre();
+    resp = validarNombre();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarHistoria();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarDescripcion();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarPrecio();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarTecnica();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarImagen();
+    if (resp == false) {
+        return false;
+    }
+
+    return true;
+
 }
 
-function ValidarFormulario() {
+function ValidarFormularioRegistro() {
     var resp = ValidarContraseña();
     if (resp == false) {
         return false;
     }
+
+    resp = validarNombre();
+    if (resp == false) {
+        return false;
+    }
     return true;
-    
 }
 
-function ValidarContraseña(){
-    
+function ValidarFormularioContacto() {
+    resp = validarNombre();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarAsunto();
+    if (resp == false) {
+        return false;
+    }
+
+    resp = validarMensaje();
+    if (resp == false) {
+        return false;
+    }
+    return true;
+}
+
+
+function ValidarContraseña() {
+
     var contra1 = document.getElementById('contra1').value;
     var contra2 = document.getElementById('contra2').value;
 
@@ -21,10 +76,125 @@ function ValidarContraseña(){
             icon: 'error',
             title: 'Contraseña',
             text: 'Las contraseñas no coinciden'
-          });
+        });
         return false;
     }
 }
 
+function validarNombre() {
+    var nombre = document.getElementById('txtNombre').value;
+    if (nombre.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Nombre',
+            text: 'El nombre no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
+
+}
+
+function validarAsunto() {
+    var asunto = document.getElementById('txtasunto').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Asunto',
+            text: 'El Asunto no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
+
+}
+
+function validarMensaje() {
+    var asunto = document.getElementById('txtmensaje').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Mensaje',
+            text: 'El Mensaje no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
+
+}
+
+function validarHistoria() {
+    var asunto = document.getElementById('txthistoria').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Historia',
+            text: 'La historia no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
+
+}
+
+function validarDescripcion() {
+    var asunto = document.getElementById('txtdescripcion').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Descripcion',
+            text: 'La descripcion no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
     
+}
+
+function validarPrecio() {
+    var asunto = document.getElementById('txtprecio').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Precio',
+            text: 'El precio no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
     
+}
+
+function validarTecnica() {
+    var asunto = document.getElementById('txttecnica').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Tecnica Usada',
+            text: 'La tecnica usada no puede estar en blanco'
+        });
+        return false;
+    }
+    return true;
+    
+}
+
+function validarImagen() {
+    var asunto = document.getElementById('formFile').value;
+    if (asunto.trim().length == 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Imagen',
+            text: 'Debe seleccionar una imagen'
+        });
+        return false;
+    }
+    return true;
+    
+}
+
+$(document).ready(function () {
+    $()
+})
+
+
